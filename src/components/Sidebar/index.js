2ac6da36672d578f6c2761c46d4cd69b7a6b24f5
Diego Bonilla <drbv27@gmail.com>
2022-04-20 17:23:45 -0500

    finish the sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,8 +1,11 @@
 import './index.scss'
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link,NavLink } from 'react-router-dom'
 import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => {
   return (
@@ -11,6 +14,35 @@ const Sidebar = () => {
             <img src={LogoS} alt="logo"/>
             <img className='sub-logo' src={LogoSubtitle} alt="slobodan"/>
         </Link>
+        <nav>
+            <NavLink exact="true" activeclassname="active" to="/">
+                <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+                <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+            </NavLink>
+        </nav>
+        <ul>
+            <li>
+                <a 
+                    target="_blank" 
+                    rel='noreferrer' 
+                    href='https://www.linkedin.com/in/diego-ricardo-bonilla-villa-7179254a/'>
+                    <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+                </a>
+            </li>
+            <li>
+                <a 
+                    target="_blank" 
+                    rel='noreferrer' 
+                    href='https://github.com/drbv27'>
+                    <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+                </a>
+            </li>
+        </ul>
     </div>
   )
 }

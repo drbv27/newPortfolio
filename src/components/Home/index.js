07b80929ab135ad1ac7626285f9202fe7e55dc33
Diego Bonilla <drbv27@gmail.com>
2022-04-23 10:16:28 -0500

    letter animation component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,14 +1,23 @@
+import { useState } from 'react'
 import LogoTitle from '../../assets/images/logo-s.png'
 import { Link } from 'react-router-dom'
 import './index.scss'
+import AnimatedLetters from '../AnimatedLetters'
 
 const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray= ['l','o','b','o','d','a','n'];
+    const jobArray=['w','e','b','','d','e','v','e','l','o','p','e','r']
     return (
         <div className="container home-page">
             <div className="text-zone">
                 <h1>Hi, <br/> I'm
                 <img src={LogoTitle} alt="developer" />
-                lobodan
+                <AnimatedLetters 
+                    letterClass={letterClass}
+                    strArray={nameArray}
+                    idx={15}
+                    />
                 <br/>
                 web developer
                 </h1>
